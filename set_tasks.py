c5efd2742119c12a8d0003e3dcc207fd401f927b
ALEX TULODZIECKI <2tuloa23@solent.ac.uk>
2023-11-07 11:30:50 +0000

    Added code to demonstrate how to create a set from a a list.

--- set_tasks.py
+++ set_tasks.py
@@ -2,8 +2,28 @@ def observed():
     observations = {'Car', 'Sky Scraper', 'Bike', 'House'}
     return observations
 
+def observed_items():
+    observations = []
+    for x in range(7):
+        observations.append(input("Please enter an observation \n"))
+    return observations
+
 def run_task1():
     print(observed())
 
+def run_task2():
+    print("Counting observations...")
+    observations = observed_items()
+    observations_set = set()
+
+    for x in observations:
+        temp_tuple = (x ,observations.count(x))
+        observations_set.add(temp_tuple)
+
+    for x in observations_set:
+        print(f"{x[0]} observed {x[1]} times")
+
+
+
 if __name__ == "__main__":
-    run_task1()
+    run_task2()
