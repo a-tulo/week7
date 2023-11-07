f84b27cdb7081b9d0f89d30ccf199c2526f8de0d
ALEX TULODZIECKI <2tuloa23@solent.ac.uk>
2023-11-07 11:11:01 +0000

    Added code to demonstrate a simple set.

---
+++ set_tasks.py
@@ -0,0 +1,9 @@
+def observed():
+    observations = {'Car', 'Sky Scraper', 'Bike', 'House'}
+    return observations
+
+def run_task1():
+    print(observed())
+
+if __name__ == "__main__":
+    run_task1()
