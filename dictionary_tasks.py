bdf174c853e4f9ab86ea4fd5150affa57360d256
ALEX TULODZIECKI <2tuloa23@solent.ac.uk>
2023-11-07 12:00:24 +0000

    Added code to demonstrate how to iterate through a dictionary.

--- dictionary_tasks.py
+++ dictionary_tasks.py
@@ -2,8 +2,33 @@ def pattern():
     sequences = {"Short Sequence" : 3, "Medium Sequence" : 2, "Long Sequenece" : 1}
     return sequences
 
+def display_keys(dict):
+    for keys in dict:
+        print(f"{keys}")
+
+
+def display_values(dict):
+    for value in dict.values():
+        print(f"{value}")
+
+def display_items(dict):
+    for key, value in dict.items():
+        print(f"{key}: {value}")
 def run():
-    print(pattern())
+    print("Dictionary:")
+    dict = pattern()
+
+    print(dict)
+    print()
+    print("Keys: ")
+    display_keys(dict)
+    print()
+    print("Values")
+    display_values(dict)
+    print()
+    print("Pairs ")
+    display_items(dict)
+
 
 if __name__ == "__main__":
     run()
